48df58ae196186d4c31f36015a0af03eb534ca1d
lundaw <kt@famulus.hu>
2021-12-01 13:10:06 +0100

    [C#] Day 1

---
+++ CSharp/Day1/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] input = File.ReadLines("input.txt").Select(int.Parse).ToArray();
+
+#region Part 1
+
+uint increaseCountPart1 = 0;
+for (var i = 1; i < input.Length; i++)
+{
+	if (input[i - 1] < input[i])
+	{
+		increaseCountPart1++;
+	}
+}
+Console.WriteLine($"[Part 1]: Increasing measurements: {increaseCountPart1}");
+
+#endregion
+
+#region Part 2
+
+uint increaseCountPart2 = 0;
+for (var i = 0; i < input.Length - 2; i++)
+{
+	if (input.Skip(i).Take(3).Sum() < input.Skip(i + 1).Take(3).Sum())
+	{
+		increaseCountPart2++;
+	}
+}
+Console.WriteLine($"[Part 2]: Increasing measurements: {increaseCountPart2}");
+
+#endregion
