c7fe11bddfdc41f65fce6ff06ebcf5cd6baf2ed1
lundaw <kt@famulus.hu>
2021-12-09 23:55:17 +0100

    [C#] Fix summation error in Day 6

--- CSharp/Day6/Program.cs
+++ CSharp/Day6/Program.cs
@@ -4,17 +4,18 @@ foreach (int fish in input) lanternfish[fish]++;
 
 long CalculateForDays(int days)
 {
+	var copyLanternfish = (lanternfish.Clone() as long[])!;
 	for (int day = 0; day < days; day++)
 	{
-		long parents = lanternfish[0];
-		for (int n = 1; n < lanternfish.Length; n++)
-			lanternfish[n - 1] = lanternfish[n];
+		long parents = copyLanternfish[0];
+		for (int n = 1; n < copyLanternfish.Length; n++)
+			copyLanternfish[n - 1] = copyLanternfish[n];
 
-		lanternfish[6] += parents;
-		lanternfish[8] = parents;
+		copyLanternfish[6] += parents;
+		copyLanternfish[8] = parents;
 	}
 
-	return lanternfish.Sum();
+	return copyLanternfish.Sum();
 }
 
 Console.WriteLine($"[Part 1]: {CalculateForDays(80)}");
