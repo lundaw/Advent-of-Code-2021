15bde17b00f279abf8bf0b0b4408d3e6bb4d1bb3
lundaw <kt@famulus.hu>
2021-12-12 00:20:29 +0100

    [C#] Day 11

---
+++ CSharp/Day11/Program.cs
@@ -0,0 +1,10 @@
+﻿using Day11;
+
+int[][] input = File.ReadLines("day11.txt")
+					.Select(row => row.Select(c => c - '0').ToArray())
+					.ToArray();
+var solution = new Solution(input);
+(int part1, int part2) = solution.Calculate();
+
+Console.WriteLine($"[Part 1]: {part1}");
+Console.WriteLine($"[Part 2]: {part2}");
---
+++ CSharp/Day11/Solution.cs
@@ -0,0 +1,73 @@
+﻿namespace Day11;
+
+public class Solution
+{
+	private readonly int[][] _input;
+
+	public Solution(int[][] input) => _input = input;
+
+	public (int, int) Calculate()
+	{
+		var flashCount = 0;
+		var i = 0;
+		while (true)
+		{
+			int flashed = Tick();
+
+			if (i++ < 100)
+				flashCount += flashed;
+
+			// Part 2
+			// If everyone flashes in sync, it means that the flash count is
+			// 100. Therefore we only need to loop until the return val is 100.
+			if (flashed == 100)
+				break;
+		}
+
+		return (flashCount, i);
+	}
+
+	private int Tick()
+	{
+		var fieldsStack = new Stack<(int, int)>();
+		var flashes = new HashSet<(int, int)>();
+		var iterateField = (int x, int y) =>
+		{
+			_input[x][y]++;
+			if (_input[x][y] > 9)
+			{
+				var field = (x, y);
+				flashes.Add(field);
+				fieldsStack.Push(field);
+			}
+		};
+
+		for (int i = 0; i < _input.Length; i++)
+			for (int j = 0; j < _input[i].Length; j++)
+				iterateField(i, j);
+
+		while (fieldsStack.Count > 0)
+		{
+			(int x, int y) = fieldsStack.Pop();
+			foreach (var neighbour in GetNeighbours(x, y))
+				if (!flashes.Contains(neighbour))
+					iterateField(neighbour.Item1, neighbour.Item2);
+		}
+
+		foreach ((int x, int y) in flashes)
+			_input[x][y] = 0;
+
+		return flashes.Count;
+	}
+
+	private List<(int, int)> GetNeighbours(int x, int y)
+	{
+		return new[]
+			{
+				(x - 1, y - 1), (x - 1, y), (x - 1, y + 1),
+				(x, y - 1), (x, y + 1),
+				(x + 1, y - 1), (x + 1, y), (x + 1, y + 1)
+			}.Where(f => f.Item1 >= 0 && f.Item1 < _input[0].Length && f.Item2 >= 0 && f.Item2 < _input.Length)
+			 .ToList();
+	}
+}
