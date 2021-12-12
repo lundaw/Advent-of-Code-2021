ab2c8494103c6de2f56887e2f7b2f1119a8c11e2
lundaw <kt@famulus.hu>
2021-12-12 01:41:55 +0100

    [Dart] Day 6 golf to single file

--- Dart/Day6/day6.dart
+++ Dart/Day6/day6.dart
@@ -1,14 +1,29 @@
 import 'dart:io';
 
-import 'solution.dart';
-
 main() async {
+  var lanternfish = {
+    0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0
+  };
   final List<String> input = await (new File("../../inputs/day6.txt")).readAsLines();
-  Solution solution = new Solution(input);
+  input.first.split(',').map((e) => int.parse(e)).forEach((e) { lanternfish[e] = lanternfish[e]! + 1; });
 
-  final int resultPart1 = solution.calculateForDays(80);
-  print("[Part 1]: $resultPart1");
+  for (int day = 0; day < 256; day++) {
+    int parents = lanternfish[0]!;
+    for (int n = 1; n < lanternfish.length; n++) {
+      lanternfish.update(n - 1, (_) => lanternfish[n]!);
+    }
 
-  final int resultPart2 = solution.calculateForDays(256);
-  print("[Part 2]: $resultPart2");
+    lanternfish.update(6, (val) => val + parents);
+    lanternfish.update(8, (_) => parents);
+
+    if (day == 80) {
+      var count = lanternfish.entries
+        .fold<int>(0, (acc, e) => acc + e.value);
+      print("[Part 1]: $count");
+    }
+  }
+
+  var finalCount = lanternfish.entries
+    .fold<int>(0, (acc, e) => acc + e.value);
+  print("[Part 2]: $finalCount");
 }
